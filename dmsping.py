15afa5adf35d0849255b3650496e3df8f4687978
Mookie Kong <mookie@outlook.com>
2016-07-01 00:42:32 +0000

    read config file location from commandline

--- dmsping.py
+++ dmsping.py
@@ -2,18 +2,24 @@
 
 import json
 import os
+import optparse
 import sys
 import requests
 
-def read_config():
-  config_location = 'config.json'
-  if os.path.isfile(config_location):
-    with open(config_location) as config_file:    
+def read_config(conf_file):
+  if os.path.isfile(conf_file):
+    with open(conf_file) as config_file:    
       config = json.load(config_file)
     return config
   else:
     print 'config file %s is missing' % config_location
     sys.exit()
+
+def get_conf_file():
+  parser = optparse.OptionParser()
+  parser.add_option('-i', action="store", help="Where are your input files located?", dest="conf_file", default="./config.json")
+  (options, args) = parser.parse_args()
+  return options.conf_file
 
 def check_host(host,url):
   response = os.system("ping -c 1 " + host)
@@ -21,7 +27,8 @@ def check_host(host,url):
     res = requests.get(url)  
   
 def run():
-  config = read_config()
+  conf_file = get_conf_file() 
+  config = read_config(conf_file)
   url = config['url']
   host = config['host']
   check_host(host,url)
